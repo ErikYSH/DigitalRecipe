0ccd3c04a821140b82f44650297cf42edd53314f
joewhitneywalker <walker.joe81@gmail.com>
2022-01-15 18:46:06 -0800

    adding routes and middleware

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
-const ctrl = require("../controllers");
+const ctrl = require("../controllers/");
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 /* ========== EXTERNAL MODULES ========== */
 const express = require("express");
-// const methodOverride = require("method-override")
+const methodOverride = require("method-override")
 
 /* ========== INTERNAL MODULES ========== */
 const routes = require('./routes');
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 // body data middleware
 app.use(express.urlencoded({ extended: true }));
 // method override middleware
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 // serve public files
 app.use(express.static("public"));
 // logger
@@ -27,6 +27,10 @@ app.use((req, res, next) => {
 	console.log(req.url, req.method);
 	next();
 });
+// const indexRoutes = require('./routes/index');
+// const userRoutes = require('./routes/user');
+// const recipeRoutes = require('./routes/recipe');
+// const food_categoryRoutes = require('./routes/food_category');
 
 
 /* ========== Router & Controllers ========== */
@@ -38,7 +42,14 @@ app.get((req,res)=>{
     res.send("404! NOT FOUND")
 })
 
+// app.use('/index', indexRoutes);
+// app.use('/user', userRoutes);
+// app.use('/recipe', recipeRoutes);
+// app.use('/food_category', food_categoryRoutes);
+
 
 app.listen(PORT, () =>{
     console.log(`Digital Recipe going ON - PORT:${PORT}`)
 })
+
+

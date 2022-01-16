5155f01ad5aa7ca718f726d5568e3d9447b0d41c
joewhitneywalker <walker.joe81@gmail.com>
2022-01-15 18:27:11 -0800

    adding in routes & testing server

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,5 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router
+
+
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const router = express.Router();
-const indexCtrl = require('../controllers/index');
-
-//app routes
-router.get('/', indexCtrl.index);
-router.get('/new', indexCtrl.new);
-router.get('/:id', indexCtrl.show);
-router.post('/', indexCtrl.create);
+const router = require('express').Router();
+const ctrl = require("../controllers");
 
 module.exports = router;

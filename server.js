5678aeba9bdeba1bce164e1b9c684f2a1617e135
joewhitneywalker <walker.joe81@gmail.com>
2022-01-16 15:18:15 -0800

    debugged our views & routes, THANK YOU ERIK FOR YOUR BRAIN

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 
 /* ========== CONFIGURATION ========== */
 const PORT = 7000;
+app.set("view engine", "ejs");
 
 /* ========== MIDDLEWARE ========== */
 // body data middleware
@@ -27,15 +28,19 @@ app.use((req, res, next) => {
 });
 
 // INTERNAL ROUTES
-const indexRoutes = require('./routes/index');
-const userRoutes = require('./routes/user');
-const recipeRoutes = require('./routes/recipe');
-const food_categoryRoutes = require('./routes/food_category');
+// const indexRoutes = require('./routes/index');
+// const userRoutes = require('./routes/user');
+// const recipeRoutes = require('./routes/recipe');
+// const food_categoryRoutes = require('./routes/food_category');
 
 
 /* ========== Router & Controllers ========== */
-app.get("/", (req,res)=>{
-    res.send("<h1> Welcome to Digital Page</h1>")
+// app.get("/", (req,res)=>{
+//     res.send("<h1> Welcome to Digital Page</h1>")
+// })
+
+app.get("/", (req, res) => {
+    res.render("index")///app.use should be used after defining routes rather than app.get
 })
 // 404 Route
 app.get((req,res)=>{
@@ -43,13 +48,13 @@ app.get((req,res)=>{
 })
 
 // VIEW ENGINE SET UP // 
-app.set("view engine", "ejs");
 
-app.use('/index', indexRoutes);
-app.use('/user', userRoutes);
-app.use('/recipe', recipeRoutes);
-app.use('/food_category', food_categoryRoutes);
 
+// app.use('/index', indexRoutes);
+// app.use('/user', userRoutes);
+// app.use('/recipe', recipeRoutes);
+// app.use('/food_category', food_categoryRoutes);
+app.use('/food_category', routes.food_category);
 
 app.listen(PORT, () => {
     console.log(`AWESOME DIGITAL RECIPE Book is Live on - PORTL:${PORT}`)
